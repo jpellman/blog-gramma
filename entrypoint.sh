76acffcc40f4c5cb716d8e609d0b1712893a41a5
John Pellman <john+github@libjpel.so>
2022-02-21 17:12:03 -0500

    Re-enable typo check for redundant spellchecks

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 for DOC in $(git diff --name-only HEAD HEAD~1 | grep 'md$'); do
 	if [ -f ${DOC} ]; then
-		gramma check -m -l en-US -d typos -p ${DOC}
+		gramma check -m -l en-US -p ${DOC}
 	fi
 done
 exit 0
