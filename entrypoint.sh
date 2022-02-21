6c808b67588d4330adf5ef3f69387339f8de5866
John Pellman <john+github@libjpel.so>
2022-02-21 16:31:47 -0500

    Troubleshooting in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-GRAMMAOUT=""
 for DOC in $(git diff --name-only HEAD HEAD~1 | grep 'md$'); do
 	if [ -f ${DOC} ]; then
-		GRAMMAOUT="${GRAMMAOUT}\n$(gramma check -m -l en-US -d typos -p ${DOC})"
+		gramma check -m -l en-US -d typos -p ${DOC}
+		echo foo
 	fi
 done
-echo "::set-output name=grammaOut::$GRAMMAOUT"
