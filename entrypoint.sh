304450434285e1436d9d15ea80b881dfa350333b
John Pellman <john+github@libjpel.so>
2022-02-21 16:42:08 -0500

    Revert "Troubleshooting in entrypoint"
    This reverts commit 6c808b67588d4330adf5ef3f69387339f8de5866.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+GRAMMAOUT=""
 for DOC in $(git diff --name-only HEAD HEAD~1 | grep 'md$'); do
 	if [ -f ${DOC} ]; then
-		gramma check -m -l en-US -d typos -p ${DOC}
-		echo foo
+		GRAMMAOUT="${GRAMMAOUT}\n$(gramma check -m -l en-US -d typos -p ${DOC})"
 	fi
 done
+echo "::set-output name=grammaOut::$GRAMMAOUT"
